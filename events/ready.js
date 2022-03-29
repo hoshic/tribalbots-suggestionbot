262fc18b04fb01847905441bd6d2c462d5040211
hoshic <robin.hoepli@gmx.ch>
2022-03-29 13:33:20 +0200

    Update ready.js
    Remove bot presence messages

--- events/ready.js
+++ events/ready.js
@@ -5,12 +5,4 @@ module.exports = client => {
     
     console.log(` :: Bot has started as : ${client.user.tag}`);
     client.user.setPresence({status: "online"}); //change to online
-    var i = 0;
-    setInterval(() => {
-        i+=1;
-        if(i === 1) client.user.setActivity(`${config.PREFIX}help | su.musicium.eu`, { type: "PLAYING" });
-        if(i === 2) client.user.setActivity(`Suggestions`, { type: "PLAYING" });
-        if(i > 3) { i = 0; client.user.setActivity(`${client.guilds.cache.reduce((c, g) => c + g.memberCount, 0)} Users`, { type: "PLAYING" });}
-       
-    }, 7500); //7.5 second delay  
 }
